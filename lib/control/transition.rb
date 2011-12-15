0b42c02e3cab7ca42ca74ff36dcdc9a1bfddb720
Jose Goncalves <zetoeu@gmail.com>
2011-12-15 10:58:02 +0000

    save is not longer overrided, using before_filter and after_filter

--- lib/control/transition.rb
+++ lib/control/transition.rb
@@ -2,6 +2,11 @@ require 'active_record'
 
 module Control
   class Transition < ActiveRecord::Base
+    validate_presence_of :workflow
+    validate_presence_of :workflow_id
+    validate_presence_of :to
+    validate_presence_of :to_id
+    
     def to_s
       "Workflow: #{workflow} || #{created_at} #{from} -> #{to}"
     end
