9f89b0439ec15f01565f2b5f6684d73b810b7ebb
Jose Goncalves <zetoeu@gmail.com>
2011-12-15 11:33:04 +0000

    transition validation

--- lib/control/transition.rb
+++ lib/control/transition.rb
@@ -2,10 +2,24 @@ require 'active_record'
 
 module Control
   class Transition < ActiveRecord::Base
-    validate_presence_of :workflow
-    validate_presence_of :workflow_id
-    validate_presence_of :to
-    validate_presence_of :to_id
+    validates :workflow, :presence => true
+    validates :workflow_id, :presence => true
+    validates :to, :presence => true
+    validates :to_id, :presence => true
+    validates :from_id, :presence => true, :if => "!from.nil?"
+    validate :validate_classes
+    
+    def validate_classes
+      if !Kernel.const_get(workflow)
+        errors.add(:workflow,'workflow class does not exist')
+      end
+      if !Kernel.const_get(to)
+        errors.add(:to,'to class does not exist')
+      end
+      if !from.blank? && !Kernel.const_get(from)
+        errors.add(:from,'from class does not exist')
+      end
+    end
     
     def to_s
       "Workflow: #{workflow} || #{created_at} #{from} -> #{to}"
